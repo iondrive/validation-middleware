6896fbabc1f3eea274f6417ecc03f4cb2fda1962
Ben Shepheard <ben@iondrive.co.uk>
2015-05-20 01:23:12 +0100

    definition: fix definition better

--- definition/validator.d.ts
+++ definition/validator.d.ts
@@ -1,13 +1,14 @@
 declare module '@iondrive/validation-middleware' {
   import express = require('express');
 
-  var logger: {
-    addFormat(name: string, format: RegExp): void;
-    addSchema(name: string, schema: {}): void;
+  module validator {
+    export function addFormat(name: string, format: RegExp): void;
+    export function addSchema(name: string, schema: {}): void;
 
-    body(schema: {}): express.RequestHandler;
-    query(schema: {}): express.RequestHandler;
-    params(schema: {}): express.RequestHandler;
-  };
-  export = logger;
+    export function body(schema: {}): express.RequestHandler
+    export function query(schema: {}): express.RequestHandler
+    export function params(schema: {}): express.RequestHandler
+  }
+
+  export = validator;
 }
