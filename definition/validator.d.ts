280e67ac75c02d92f6afd90705a9fbde83dbba60
Ben Shepheard <ben@iondrive.co.uk>
2015-05-20 01:05:37 +0100

    definition: fix definition errors

--- definition/validator.d.ts
+++ definition/validator.d.ts
@@ -1,7 +1,7 @@
-import express = require('express');
-
 declare module '@iondrive/validation-middleware' {
-  interface logger {
+  import express = require('express');
+
+  var logger: {
     addFormat(name: string, format: RegExp): void;
     addSchema(name: string, schema: {}): void;
 
